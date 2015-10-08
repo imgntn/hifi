6cc299b4a9ce18329e6c15d7a33be27d5ed4d15c
James Pollack <jamesbradenpollack@gmail.com>
2015-10-08 19:36:40 +0100

    javascript build tools

---
+++ javascript_build/gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var insert = require('gulp-insert');
+var fs = require('fs');
+
+var jsLintGlobals = require('./jsLintGlobals');
+var linterGlobals = [jsLintGlobals.hifi_API, jsLintGlobals.hifi_utilites];
+var linterGlobalString = "/*global " + linterGlobals.join(",") + "*/" + '\n';
+
+var jsLicense = require('./jsLicense');
+var jsAuthorCopyright = require('./jsLicense');
+
+gulp.task('addLinterGlobals', function() {
+    console.log('adding linter globals');
+    gulp.src('../examples/**/*.js')
+        .pipe(insert.prepend(linterGlobalString))
+        .pipe(gulp.dest('tempjs'));
+});
+
+gulp.task('addFilePath', function() {
+    console.log('adding file path ');
+    gulp.src('../examples/**/*.js')
+        .pipe(insert.transform(function(contents, file) {
+            var filename = '// local file: ' + file.path + '\n';
+            return filename + contents;
+        }))
+        .pipe(gulp.dest('tempjs'));
+});
+
+gulp.task('addLicense', function() {
+    console.log('adding license');
+    gulp.src('../examples/**/*.js')
+        .pipe(insert.transform(function(contents, file) {
+            var license = jsLicense.apache2 + '\n';
+            return license + contents;
+        }))
+        .pipe(gulp.dest('tempjs'));
+});
+
+gulp.task('addAuthorCopyright', function() {
+    console.log('adding author and copyright');
+    gulp.src('../examples/**/*.js')
+        .pipe(insert.transform(function(contents, file) {
+            var author = jsAuthorCopyright.author + '\n';
+            var copyright = jsAuthorCopyright.copyRight + '\n';
+            return author + copyright + contents;
+        }))
+        .pipe(gulp.dest('tempjs'));
+});
+
+gulp.task('createEntityTemplate',function(){
+    fs.writeFileSync('templates/entityScript.js', headerTemplate + entityTemplate);
+})
+
+gulp.task('createACTemplate',function(){
+    fs.writeFileSync('templates/AC.js', headerTemplate + entityTemplate);
+})
+gulp.task('createClientTemplate',function(){
+    fs.writeFileSync('templates/clientScript.js', headerTemplate + entityTemplate);
+})
