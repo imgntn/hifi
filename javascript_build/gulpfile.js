9a48f7a3d4bc7032e982c1c019e0ea1f04eeaa52
James B. Pollack <jamesbradenpollack@gmail.com>
2015-10-08 12:42:21 -0700

    add comments, set to production

--- javascript_build/gulpfile.js
+++ javascript_build/gulpfile.js
@@ -1,25 +1,31 @@
 var gulp = require('gulp');
 var insert = require('gulp-insert');
 var deleteLines = require('gulp-delete-lines');
-// var fs = require('fs');
+var fs = require('fs');
 
+//we store our globals in an easy to access file.  join them together to make a string.
 var jsLintGlobals = require('./jsLintGlobals');
 var linterGlobals = [jsLintGlobals.hifi_API, jsLintGlobals.hifi_utilites];
 var linterGlobalString = "/*global " + linterGlobals.join(",") + "*/" + '\n';
 
-var jsLicense = require('./jsLicense');
-var jsAuthorCopyright = require('./jsLicense');
-
+//gulp task to delete and then add globals
 gulp.task('updateLinterGlobals', function() {
     gulp.src('../examples/**/*.js')
+        //regex that matches /*global and deletes
         .pipe(deleteLines({
             'filters': [
                 /\/{1}\*{1}(global)/
             ]
         }))
+        //add the global
         .pipe(insert.prepend(linterGlobalString))
-        .pipe(gulp.dest('tempjs'));
+        //send it to the destination
+        .pipe(gulp.dest('../examples'));
 });
+
+//for the future, we will compose a header file from the file name, license, author, copyright, etc
+var jsLicense = require('./jsLicense');
+var jsAuthorCopyright = require('./jsLicense');
 
 gulp.task('addFilePath', function() {
     console.log('adding file path ');
@@ -28,7 +34,7 @@ gulp.task('addFilePath', function() {
             var filename = '// local file: ' + file.path + '\n';
             return filename + liccontents;
         }))
-        .pipe(gulp.dest('tempjs'));
+        .pipe(gulp.dest('../examples'));
 });
 
 gulp.task('addLicense', function() {
@@ -38,7 +44,7 @@ gulp.task('addLicense', function() {
             var license = jsLicense.apache2 + '\n';
             return license + contents;
         }))
-        .pipe(gulp.dest('tempjs'));
+        .pipe(gulp.dest('../examples'));
 });
 
 gulp.task('addAuthorCopyright', function() {
@@ -49,9 +55,10 @@ gulp.task('addAuthorCopyright', function() {
             var copyright = jsAuthorCopyright.copyRight + '\n';
             return author + copyright + contents;
         }))
-        .pipe(gulp.dest('tempjs'));
+        .pipe(gulp.dest('../examples'));
 });
 
+//we will use the header template plus some object templates to create easy to use templates for users to start from
 gulp.task('createEntityTemplate', function() {
     fs.writeFileSync('templates/entityScript.js', headerTemplate + entityTemplate);
 })
