245a3141821e8ec16654c81238b82a494c64c9b2
James B. Pollack <jamesbradenpollack@gmail.com>
2015-10-10 10:44:09 -0700

    entity recorder

---
+++ examples/entityScripts/entityRecorder.js
@@ -0,0 +1,72 @@
+//
+//  entityRecorder
+//
+//  This script records the properties of the entity it is attached to.  It sends in batches, which can be tuned to fit your reesolution / perofrmance requirements.
+//  Created by James B. Pollack @imgntn on 10/10/2015
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+(function() {
+
+    var ENDPOINT_URL = "";
+
+    var BATCH_SIZE = 5;
+
+    var batch = [];
+
+    var _this;
+
+    var recorderInterval;
+
+    function Recorder() {
+      _this = this;
+      return;
+    }
+
+    Recorder.prototype = {
+      init: function() {
+        var RECORD_EVERY = 1000; // 1 seconds
+        var batchCount = 0;
+
+        recorderInterval = Script.setInterval(function() {
+
+          if (batchCount === BATCH_SIZE) {
+            sendBatchToEndpoint(batch);
+            batchCount = 0;
+          }
+          Stats.forceUpdateStats();
+          batch.push(getProperties());
+          batchCount++;
+        }, RECORD_EVERY);
+
+      },
+      preload: function() {
+        this.entityID = entityID;
+      },
+      getProperties: function() {
+        return {
+          entityID: _this.entityID,
+          properties: Entities.getEntityProperties(_this.entityID)
+        }
+      }
+
+
+      function sendBatchToEndpoint(batch) {
+        // print('SEND BATCH TO ENDPOINT');
+        var req = new XMLHttpRequest();
+        req.open("POST", ENDPOINT_URL, false);
+        req.send(JSON.stringify(batch));
+        batch = [];
+      }
+
+      return new Recorder();
+    })
+
+  function deleteInterval() {
+    Script.clearInterval(recorderInterval);
+    Entities.deletingEntity.disconnect(deleteInterval);
+  }
+  Entities.deletingEntity.connect(deleteInterval);
