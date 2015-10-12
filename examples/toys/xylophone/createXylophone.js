2291c9573577d8e3aff5d39c0a43e815e16ed46f
James B. Pollack <jamesbradenpollack@gmail.com>
2015-10-12 10:40:23 -0700

    xylophone review for bugs

--- examples/toys/xylophone/createXylophone.js
+++ examples/toys/xylophone/createXylophone.js
@@ -8,9 +8,9 @@ var xylophoneSounds = [];
 
 var keyEntities = [];
 var KEY_SPACING = {
-	x: 0.16,
-	y: 0,
-	z: 0
+    x: 0.16,
+    y: 0,
+    z: 0
 };
 
 var mallets = [];
@@ -20,165 +20,163 @@ var xylophoneBase;
 //make keys
 //make mallets
 var center = Vec3.sum(Vec3.sum(MyAvatar.position, {
-	x: 0,
-	y: 0.5,
-	z: 0
+    x: 0,
+    y: 0.5,
+    z: 0
 }), Vec3.multiply(1, Quat.getFront(Camera.getOrientation())));
 var baseStartPosition = {
-	x: 546.10,
-	y: 495.34,
-	z: 506.50
+    x: 546.10,
+    y: 495.34,
+    z: 506.50
 };
 
 var baseStartPosition = center;
 
 function createXylophoneBase() {
-	var properties = {
-		type: 'Model',
-		modelURL: XYLOPHONE_MODEL_URL,
-		dimensions: {
-			x: 0.64,
-			y: 0.09,
-			z: 0.34,
-		},
-		collisionsWillMove: false,
-		gravity: {
-			x: 0,
-			y: -9.8,
-			z: 0
-		},
-		position: baseStartPosition,
-	}
+    var properties = {
+        type: 'Model',
+        modelURL: XYLOPHONE_MODEL_URL,
+        dimensions: {
+            x: 0.64,
+            y: 0.09,
+            z: 0.34,
+        },
+        collisionsWillMove: false,
+        gravity: {
+            x: 0,
+            y: -9.8,
+            z: 0
+        },
+        position: baseStartPosition,
+    }
 
-	xylophoneBase = Entities.addEntity(properties);
+    xylophoneBase = Entities.addEntity(properties);
 }
 
 var keyInfo = [{
-	note: 'C2',
-	keyLength: 0.3556,
+    note: 'C2',
+    keyLength: 0.3556,
 }, {
-	note: 'D2',
-	keyLength: 0.3429,
+    note: 'D2',
+    keyLength: 0.3429,
 }, {
-	note: 'E2',
-	keyLength: 0.3302,
+    note: 'E2',
+    keyLength: 0.3302,
 }, {
-	note: 'F2',
-	keyLength: 0.3175,
+    note: 'F2',
+    keyLength: 0.3175,
 }, {
-	note: 'G2', 
-	keyLength: 0.3048,
+    note: 'G2', 
+    keyLength: 0.3048,
 }, {
-	note: 'A2',
-	keyLength: 0.2921,
+    note: 'A2',
+    keyLength: 0.2921,
 }, {
-	note: 'B2',
-	keyLength: 0.2794,
+    note: 'B2',
+    keyLength: 0.2794,
 }, {
-	note: 'C3',
-	keyLength: 0.2667,
+    note: 'C3',
+    keyLength: 0.2667,
 }];
 
 function createXylophoneKeys() {
 
-	var rotation = Quat.fromPitchYawRollDegrees(0, -56, 0);
+    var rotation = Quat.fromPitchYawRollDegrees(0, -56, 0);
 
-	keyInfo.forEach(function(xyloKey, index) {
-		//var position = Vec3.sum(baseStartPosition, Vec3.multiply(index, KEY_SPACING));
+    keyInfo.forEach(function(xyloKey, index) {
+        var vHat = Quat.getRight(rotation);
+        var spacer = 0.16 * index;
+        var multiplier = Vec3.multiply(spacer, vHat);
+        var position = Vec3.sum(baseStartPosition, multiplier);
 
-		var vHat = Quat.getRight(rotation);
-		var spacer = 0.16 * index;
-		var multiplier = Vec3.multiply(spacer, vHat);
-		var position = Vec3.sum(baseStartPosition, multiplier);
+        var properties = {
+            type: 'Box',
+            shapeType: 'Box',
+            name: 'Xylophone Key ' + xyloKey.note,
+            script: XYLOPHONE_KEY_SCRIPT_URL,
+            dimensions: {
+                x: 0.1008,
+                y: 0.0454,
+                z: xyloKey.keyLength * 2
+            },
+            position: position,
+            rotation: rotation,
+            color: {
+                red: 0,
+                green: 0,
+                blue: 255
+            },
+            restitution: 0,
+            collisionsWillMove: false,
+            gravity: {
+                x: 0,
+                y: -9.8,
+                z: 0
+            },
+            linearDamping: 1,
+            angularDamping: 1,
+            userData: JSON.stringify({
+                resetMe: {
+                    resetMe: true
+                },
+                grabbableKey: {
+                    grabbable: false
+                },
+                soundKey: {
+                    soundURL: SOUND_BASE_URL + xyloKey.note + ".L.wav"
+                }
+            })
+        };
 
-		var properties = {
-			type: 'Box',
-			shapeType: 'Box',
-			name: 'Xylophone Key ' + xyloKey.note,
-			script: XYLOPHONE_KEY_SCRIPT_URL,
-			dimensions: {
-				x: 0.1008,
-				y: 0.0454,
-				z: xyloKey.keyLength * 2
-			},
-			position: position,
-			rotation: rotation,
-			color: {
-				red: 0,
-				green: 0,
-				blue: 255
-			},
-			restitution: 1,
-			collisionsWillMove: false,
-			gravity: {
-				x: 0,
-				y: -9.8,
-				z: 0
-			},
-			linearDamping: 1,
-			angularDamping: 1,
-			userData: JSON.stringify({
-				resetMe: {
-					resetMe: true
-				},
-				grabbableKey: {
-					grabbable: false
-				},
-				soundKey: {
-					soundURL: SOUND_BASE_URL + xyloKey.note + ".L.wav"
-				}
-			})
-		};
+        //print('PROPS::'+JSON.stringify(properties))
 
-		//print('PROPS::'+JSON.stringify(properties))
+        var xyloPhoneKey = Entities.addEntity(properties);
+        keyEntities.push(xyloPhoneKey);
 
-		var xyloPhoneKey = Entities.addEntity(properties);
-		keyEntities.push(xyloPhoneKey);
-
-	})
+    })
 }
 
 function createMallets() {
 
-	var properties = {
-		type: 'Model',
-		name: 'Xylophone Mallet',
-		modelURL: MALLET_MODEL_URL,
-		dimensions: {
-			x: 0.46,
-			y: 0.04,
-			z: 0.04
-		},
-		restitution: 1,
-		collisionsWillMove: true,
-		position: baseStartPosition,
-		shapeType: 'compound',
-		compoundShapeURL: MALLET_COLLISION_HULL_URL,
-		userData: JSON.stringify({
-			resetMe: {
-				resetMe: true
-			},
-			grabbableKey: {
-				invertSolidWhileHeld: true
-			}
-		})
-	}
+    var properties = {
+        type: 'Model',
+        name: 'Xylophone Mallet',
+        modelURL: MALLET_MODEL_URL,
+        dimensions: {
+            x: 0.46,
+            y: 0.04,
+            z: 0.04
+        },
+        restitution: 0,
+        collisionsWillMove: true,
+        position: baseStartPosition,
+        shapeType: 'compound',
+        compoundShapeURL: MALLET_COLLISION_HULL_URL,
+        userData: JSON.stringify({
+            resetMe: {
+                resetMe: true
+            },
+            grabbableKey: {
+                invertSolidWhileHeld: true
+            }
+        })
+    }
 
-	var firstMallet = Entities.addEntity(properties);
-	// var secondMallet = Entities.addEntity(properties);
-	mallets.push(firstMallet);
-	//	mallets.push(secondMallet);
+    var firstMallet = Entities.addEntity(properties);
+    // var secondMallet = Entities.addEntity(properties);
+    mallets.push(firstMallet);
+    //  mallets.push(secondMallet);
 }
 
 function cleanup() {
-	while (mallets.length > 0) {
-		Entities.deleteEntity(mallets.pop());
-	}
-	while (keyEntities.length > 0) {
-		Entities.deleteEntity(keyEntities.pop());
-	}
+    while (mallets.length > 0) {
+        Entities.deleteEntity(mallets.pop());
+    }
+    while (keyEntities.length > 0) {
+        Entities.deleteEntity(keyEntities.pop());
+    }
 
-	Entities.deleteEntity(xylophoneBase);
+    Entities.deleteEntity(xylophoneBase);
 }
 
 Script.scriptEnding.connect(cleanup);
--- examples/toys/xylophone/xylophoneKey.js
+++ examples/toys/xylophone/xylophoneKey.js
@@ -1,67 +1,64 @@
 (function() {
-	Script.include("../../libraries/utils.js");
+    Script.include("../../libraries/utils.js");
 
-	var defaultSoundData = {
-		soundURL: null
-	};
+    var defaultSoundData = {
+        soundURL: null
+    };
 
-	var _this;
+    var _this;
 
-	function XylophoneKey() {
-		_this = this;
-		return;
-	}
-	var checkPlayingInterval = null;
-	XylophoneKey.prototype = {
-		isPlaying: false,
-		sound: null,
-		injector: null,
-		collisionWithEntity: function(thisEntity, otherEntity, collision) {
-			var soundOptions = {
-				localOnly: true,
-				position: collision.contactPoint,
-				volume: 0.5
-			};
-
-
-			if (this.sound !== null && this.isPlaying !== true) {
-				this.injector = Audio.playSound(this.sound, soundOptions);
-				this.isPlaying = true;
-				print('INJECTOR'+this.injector)
-				// if (checkPlayingInterval === null) {
-				// 	checkPlayingInterval = Script.setInterval(checkIsPlaying, 1000);
-				// }
-
-			}
-		},
-
-		preload: function(entityID) {
-			this.entityID = entityID;
-			var soundData = getEntityCustomData("soundKey", entityID, defaultSoundData);
-			this.sound = SoundCache.getSound(soundData.soundURL);
-
-		}
-
-	};
-
-	function deleteEntity(entityID) {
-		if (entityID === _this.entityID) {
-			Script.clearInterval(checkPlayingInterval);
-			Entities.deletingEntity.disconnect(deleteEntity);
-		}
-	};
-
-	function checkIsPlaying() {
-		if (_this.injector !== null && _this.sound !== null) {
-			print('isPlaying???', _this.injector)
-				//_this.isPlaying = _this.injector.isPlaying;
+    function XylophoneKey() {
+        _this = this;
+        return;
+    }
+    var checkPlayingInterval = null;
+    XylophoneKey.prototype = {
+        isPlaying: false,
+        sound: null,
+        injector: null,
+        collisionWithEntity: function(thisEntity, otherEntity, collision) {
+            var soundOptions = {
+                localOnly: true,
+                position: collision.contactPoint,
+                volume: 0.5
+            };
 
 
-		}
-	}
-	Entities.deletingEntity.connect(deleteEntity);
+            if (this.sound !== null && this.isPlaying !== true) {
+                this.injector = Audio.playSound(this.sound, soundOptions);
+                this.isPlaying = true;
+                if (checkPlayingInterval === null) {
+                    checkPlayingInterval = Script.setInterval(checkIsPlaying, 100);
+                }
 
-	return new XylophoneKey();
+            }
+        },
+
+        preload: function(entityID) {
+            this.entityID = entityID;
+            var soundData = getEntityCustomData("soundKey", entityID, defaultSoundData);
+            this.sound = SoundCache.getSound(soundData.soundURL);
+        }
+    };
+
+    function deleteEntity(entityID) {
+        if (entityID === _this.entityID) {
+            Script.clearInterval(checkPlayingInterval);
+            Entities.deletingEntity.disconnect(deleteEntity);
+        }
+    };
+
+    function checkIsPlaying() {
+        if (_this.injector !== null && _this.sound !== null) {
+            //print('isPlaying???', _this.injector.isPlaying)
+                _this.isPlaying = _this.injector.isPlaying;
+
+
+        }
+    }
+    Entities.deletingEntity.connect(deleteEntity);
+
+    return new XylophoneKey();
 
 
 });
