7aad780a867c42e6600d699c1ab4d70e91257a3d
James B. Pollack <jamesbradenpollack@gmail.com>
2015-11-06 22:13:47 -0800

    Update behavior3.js

--- examples/libraries/behavior3.js
+++ examples/libraries/behavior3.js
@@ -22,6 +22,8 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+//
+// Adapted for High Fidelity by James B. Pollack @imgnt on 11/6/2015
 
 loadb3 = function(){
     return b3
