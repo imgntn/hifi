91ee53f84555c0eac9096dcc512af6114e74bd74
James B. Pollack <jamesbradenpollack@gmail.com>
2015-11-27 15:21:01 -0800

    start tower defense

---
+++ examples/example/games/tower_d/creatureSteering.js
@@ -0,0 +1,66 @@
+function arrive(thisEntity, target) {
+
+  var targetPosition = Entities.getEntityProperties(target, "position").position;
+  var properties = Entities.getEntityProperties(thisEntity, ["position", "velocity"]);
+  var location = properties.position;
+  var velocity = properties.velocity;
+  var MAX_SPEED = 12;
+  var MAX_FORCE = 8;
+  var ARRIVAL_DISTANCE = 3;
+
+  var desired = Vec3.subtract(targetPosition, location);
+  var d = Vec3.length(desired);
+  desired = Vec3.normalize(desired);
+  if (d < ARRIVAL_DISTANCE) {
+    var m = scale(d, 0, ARRIVAL_DISTANCE, 0, MAX_SPEED);
+  } else {
+    desired = Vec3.multiply(MAX_SPEED, desired);
+
+  }
+  var steer = Vec3.subtract(desired, velocity);
+  var steerVector = new V3(desired.x, 0, desired.z);
+  steer = steerVector.limit(MAX_FORCE)
+
+  return steer
+}
+
+
+function V3(x, y, z) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  return
+}
+
+V3.prototype.length = function() {
+  return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+};
+
+V3.prototype.limit = function(s) {
+  var len = this.length();
+
+  if (len > s && len > 0) {
+    this.scale(s / len);
+  }
+
+  return this;
+};
+
+V3.prototype.scale = function(f) {
+  this.x *= f;
+  this.y *= f;
+  this.z *= f;
+  return this;
+};
+
+var v3 = new V3();
+
+var scale = function(value, min1, max1, min2, max2) {
+  return min2 + (max2 - min2) * ((value - min1) / (max1 - min1));
+}
+
+loadSteer = function() {
+  return {
+    arrive: arrive
+  }
+}
---
+++ examples/example/games/tower_d/tower_d.js
@@ -0,0 +1,165 @@
+// tower defense game w/bow
+
+//put tower in center
+//put spawners in ring around the tower
+//spawn betas that walk / crawl (seek to tower)
+//parent head collision detector and body collision detector capsules to beta
+//increase spawn rate over time (and with number of players)
+//once betas reach the tower they start climbing
+//spawn one bow per player
+//adjust the cooldown time mechanic
+//when an arrow collides with a runner head, it is defeated
+//when an arrow collides with a runner body, it slows to crawl
+//when an arrow collides with a crawler body, it is defeated
+//when an arrow collides with a climber, it is defeated
+//change animation and collider location with 
+
+
+Script.include('creatureSteering.js');
+var steer = loadSteer();
+
+var CREATURE_SCRIPT_URL = '';
+var BOW_SCRIPT_URL = '';
+
+var BOTTOM_OF_TOWER_TARGET = {
+	x: 0,
+	y: 0,
+	z: 0
+};
+
+var TOP_OF_TOWER_TARGET {
+	x: 0,
+	y: 0,
+	z: 0
+};
+
+var TOWER_MODEL_URL = '';
+var CREATURE_MODEL_URL = '';
+var BOW_MODEL_URL = '';
+
+var TOWER_DIMENSIONS = {
+	x: 0,
+	y: 0,
+	z: 0
+};
+var CREATURE_DIMENSIONS = {
+	x: 0,
+	y: 0,
+	z: 0
+};
+var BOW_DIMENSIONS = {
+	x: 0,
+	y: 0,
+	z: 0
+};
+
+var NUMBER_OF_SPAWNERS = 12;
+var SPAWNER_RADIUS_X = 50;
+var SPAWNER_RADIUS_y\ Y = 50;
+
+var MOVE_SEEKING_CREATURES_INTERVAL = 100;
+var MOVE_CLIMBING_CREATURES_INTERVAL = 250;
+
+var CREATURE_CLIMB_SPEED = 1;
+
+App.prototype = {
+	tower: null,
+	ground: null,
+	spawners: [],
+	creatures: [],
+	timers: [],
+	moveCreatureTowardTarget: function(creature, target) {
+		var seek = steer.arrive(creature, target);
+		Entities.editEntity(creature, {
+			velocity: seek
+		});
+	},
+	moveCreatureUpWall: function(creature) {
+		Entities.editEntity(creature, {
+			velocity: {
+				x: 0,
+				y: 1,
+				z: 0
+			}
+		});
+	},
+	setupScene: function() {
+		this.ground = this.createGround();
+		this.tower = this.createTower();
+		distribute(NUMBER_OF_SPAWNERS, SPAWNER_RADIUS_X, SPAWNER_RADIUS_Y);
+
+	},
+	createGround: function(position) {
+
+	},
+	createTower: function(position) {
+		var tower;
+		return Entities.addEntity(tower);;
+	},
+	createSpawner: function(position) {
+		var spawner;
+		return Entities.addEntity(spawner);;
+	},
+	createCreature: function(position) {
+		var creature;
+		return Entities.addEntity(creature);
+	},
+	createBow: function(position) {
+
+	},
+	changeAnimationToType: function(creature, animationType) {
+
+	},
+	distribute: function(_numberOfPoints, radiusX, radiusY) {
+
+		var IS_VERTICAL = false;
+
+		var centrePos = {
+			x: 100,
+			y: 100,
+			z: 100
+		}
+
+		for (var pointNum = 0; pointNum < _numberOfPoints; pointNum++) {
+			// "i" now represents the progress around the circle from 0-1
+			// we multiply by 1.0 to ensure we get a fraction as a result.
+			var i = (pointNum * 1.0) / _numberOfPoints;
+
+			// get the angle for this step (in radians, not degrees)
+			var angle = i * Math.PI * 2;
+
+			// the X &amp; Y position for this angle are calculated using Sin &amp; Cos
+			var x = Math.sin(angle) * radiusX;
+			var y = Math.cos(angle) * radiusY;
+
+
+			if (IS_VERTICAL) {
+				var pos = {
+					x: x + centrePos.x,
+					y: y + centrePos.y,
+					z: 0 + centrePos.z
+				}
+
+			} else {
+
+				var pos = {
+					x: x + centrePos.x,
+					y: 0 + centrePos.z,
+					z: y + centrePos.y
+				}
+			}
+
+			print('CREATE AT:' + pos.x + "/" + pos.y + "/" + pos.z);
+
+			this.spawners.push(this.createSpawner(pos));
+
+		}
+
+
+	},
+	cleanupTimers: function() {
+		while (this.timers.length > 0) {
+			Script.clearInterval(this.timers.pop())
+		}
+	}
+}
