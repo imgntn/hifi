78b16810eaf92d314caf13e7ec9942adbe666ffe
James B. Pollack <jamesbradenpollack@gmail.com>
2016-01-05 16:18:19 -0800

    cleanup

--- examples/toybox/ping_pong_gun/wallTarget.js
+++ examples/toybox/ping_pong_gun/wallTarget.js
@@ -11,6 +11,7 @@
 /*global print, MyAvatar, Entities, AnimationCache, SoundCache, Scene, Camera, Overlays, Audio, HMD, AvatarList, AvatarManager, Controller, UndoStack, Window, Account, GlobalServices, Script, ScriptDiscoveryService, LODManager, Menu, Vec3, Quat, AudioDevice, Paths, Clipboard, Settings, XMLHttpRequest, randFloat, randInt */
 (function() {
 
+    var SOUND_URL = "http://hifi-public.s3.amazonaws.com/sounds/Clay_Pigeon_02.L.wav";
 
     function Target() {
         return;
@@ -20,7 +21,6 @@
         hasBecomeActive: false,
         preload: function(entityID) {
             this.entityID = entityID;
-            var SOUND_URL = "http://hifi-public.s3.amazonaws.com/sounds/Clay_Pigeon_02.L.wav";
             this.hitSound = SoundCache.getSound(SOUND_URL);
         },
         collisionWithEntity: function(me, otherEntity) {
@@ -51,6 +51,5 @@
         }
     };
 
-    // entity scripts always need to return a newly constructed object of our type
     return new Target();
 });
