35839a0b3418c91f5da92b8fcc310be8eba79834
James B. Pollack <jamesbradenpollack@gmail.com>
2016-02-25 13:04:28 -0800

    test

---
+++ unpublishedScripts/DomainContent/Playa/neonLights/entityCountTest.js
@@ -0,0 +1,71 @@
+var THROTTLE = true;
+var THROTTLE_SPREAD = 0;
+var throttleRate = 250
+var sinceLastUpdate = 0;
+var initialized = false;
+var octreeQueryInterval;
+
+var basePosition = {
+    x: 0,
+    y: 0,
+    z: 0
+}
+
+function update(deltaTime) {
+    if (THROTTLE === true) {
+        sinceLastUpdate = sinceLastUpdate + deltaTime * 100;
+        if (sinceLastUpdate > throttleRate) {
+            throttleRate = Math.random() * THROTTLE_SPREAD;
+            sinceLastUpdate = 0;
+        } else {
+            return;
+        }
+    }
+
+    if (!initialized) {
+        print("checking for servers...");
+        if (Entities.serversExist() && Entities.canRez()) {
+            print("servers exist -- makeAll...");
+            Entities.setPacketsPerSecond(6000);
+            print("PPS:" + Entities.getPacketsPerSecond());
+            initialized = true;
+        }
+        return;
+    }
+
+    countEntities()
+
+}
+
+function setUpEntityViewer() {
+    EntityViewer.setPosition(basePosition);
+    EntityViewer.setKeyholeRadius(32000);
+    octreeQueryInterval = Script.setInterval(function() {
+        // print('looking in the octree')
+        EntityViewer.queryOctree();
+    }, 200);
+}
+
+
+function countEntities() {
+
+    if (EntityViewer.getOctreeElementsCount() <= 1) {
+        Script.setTimeout(function() {
+            deleteAllFlickeringLights();
+        }, 1000)
+        print('JBP countEntities 2')
+        return;
+    }
+
+    var results = Entities.findEntities(basePosition, 32000);
+    print('HOW MANY TOTAL:: ' + results.length)
+
+}
+
+setUpEntityViewer();
+Script.update.connect(update);
+
+Script.scriptEnding.connect(function() {
+    Script.update.disconnect(update);
+    Script.clearInterval(octreeQueryInterval)
+})
